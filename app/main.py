e81675bf7bee3df0c37c98b9f55806b54f66c412
DilanJT <dicmandilan@gmail.com>
2021-06-16 20:40:44 +0530

    route functions read update and delete added

--- app/main.py
+++ app/main.py
@@ -40,5 +40,43 @@ def readAssetsList():
     noncurrentassets = NonCurrentAsset.query.all()
     return render_template("assetlist.html", noncurrentassets=noncurrentassets)
 
+@app.route('assets/<int:id>')
+def readAsset(id):
+    asset = NonCurrentAsset.query.filter_by(id = id).first()
+    if asset:
+        return render_template('asset.html', asset = asset)
+    return f"Cannot find the asset with id={id}"
+
+@app.route('assets/<int:id>/update', methods= ['GET', 'POST'])
+def updateAsset(id):
+    asset = NonCurrentAsset.query.filter_by(id = id).first()
+    if request.method == 'POST':
+        if asset:
+            datab.session.delete(asset)
+            datab.session.commit()
+            name = request.form['name']
+            assetType = request.form["assetType"]
+            assetValue = request.form["assetValue"]
+            annualDepreciation = request.form["annualDepreciation"]
+            yearsUsed = request.form["yearsUsed"]
+
+            asset = NonCurrentAsset(name=name, assetType=assetType, assetValue=int(assetValue), annualDepreciation=int(annualDepreciation), yearsUsed=int(yearsUsed))
+            datab.session.add(asset)
+            datab.session.commit()
+            return redirect(f'/data/{id}')
+        return f"Cannot find the asset with id={id}"
+    return render_template("update.html", asset = asset)
+
+@app.route('assets/<int:id>/delete', methods=['GET', 'POST'])
+def deleteAsset(id):
+    asset = NonCurrentAsset.query.filter_by(id=id).first()
+    if request.method == "POST":
+        if asset:
+            datab.session.delete(asset)
+            datab.session.commit()
+            return redirect('/assets')
+
+    return render_template('delete.html')
+
 
 app.run(host="localhost", port="5000")
